b0061342b2e3416e32cae6125ed97459dad56624
Suronjit Sutradhar <suronjit02@gmail.com>
2026-06-09 14:09:29 +0600

    container with most water : leetcode 11

---
+++ 11-Container-With-Most-Water.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+const maxArea = (height) => {
+  let left = 0;
+  let right = height.length - 1;
+  let maxArea = 0;
+
+  while (left < right) {
+    const currentArea = Math.min(height[left], height[right]) * (right - left);
+
+    maxArea = Math.max(maxArea, currentArea);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return maxArea;
+};
