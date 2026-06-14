3a1397a41b54e6646e52136a103d19efd4375cc4
Suronjit Sutradhar <suronjit02@gmail.com>
2026-06-14 22:54:18 +0600

    pow (x, n) : leetcode 50

---
+++ 50-Pow.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+const myPow = (x, n) => {
+  if (n === 0) return 1;
+
+  if (n < 0) {
+    x = 1 / x;
+    n = -n;
+  }
+
+  if (n % 2 === 0) {
+    return myPow(x * x, n / 2);
+  } else {
+    return x * myPow(x * x, (n - 1) / 2);
+  }
+};
