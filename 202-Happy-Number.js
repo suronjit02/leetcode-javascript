acf1a14e7134a06f78f8684d99b39dd6bf7c8e04
Suronjit Sutradhar <suronjit02@gmail.com>
2026-06-16 22:14:24 +0600

    happy number : leetcode 202

---
+++ 202-Happy-Number.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const isHappy = (n) => {
+  const seen = new Set();
+
+  const getNext = (num) => {
+    let sum = 0;
+
+    while (num > 0) {
+      const digit = num % 10;
+      sum += digit * digit;
+      num = Math.floor(num / 10);
+    }
+
+    return sum;
+  };
+
+  while (n !== 1 && !seen.has(n)) {
+    seen.add(n);
+    n = getNext(n);
+  }
+
+  return n === 1;
+};
