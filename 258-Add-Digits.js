15eb23a38b25cd3e01e8fc9715829e5384af31de
Suronjit Sutradhar <suronjit02@gmail.com>
2026-06-22 14:27:20 +0600

    add digits : leetcode 258

---
+++ 258-Add-Digits.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+const addDigits = (num) => {
+  if (num < 10) {
+    return num;
+  }
+
+  let sum = 0;
+
+  while (num > 0) {
+    sum += num % 10;
+    num = Math.floor(num / 10);
+  }
+
+  return addDigits(sum);
+};
