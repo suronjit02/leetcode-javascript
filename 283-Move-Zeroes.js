6b7267ff17d674cf6f1d232b8eb7ad6040097860
Suronjit Sutradhar <suronjit02@gmail.com>
2026-06-24 18:46:05 +0600

    move zeroes : leetcode 283

---
+++ 283-Move-Zeroes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const moveZeroes = (nums) => {
+  let lastNonZeroFoundAt = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[lastNonZeroFoundAt] = nums[i];
+      lastNonZeroFoundAt++;
+    }
+  }
+  for (let i = lastNonZeroFoundAt; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+
+  
+};
